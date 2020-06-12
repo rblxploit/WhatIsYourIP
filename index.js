8a721b3f7443a7ffc456e60257e340874fe4d59b
WaaberiIbrahim <waaberi.mohamed.ibrahim@gmail.com>
2020-06-12 15:29:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  var ip =
+    (req.headers["x-forwarded-for"] || "").split(",").pop().trim() ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+  res.send(ip);
+});
+
+app.listen(3000, () => {
+  console.log("Server started at port 3000.");
+});
